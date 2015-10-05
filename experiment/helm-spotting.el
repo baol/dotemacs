1f1a342635860f4ddfd3dd6e6c73445fb4f905bf
Mirko Maischberger <mirko.maischberger@here.com>
2015-10-06 00:21:52 +0200

    Trying out helm

---
+++ experiment/helm-spotting.el
@@ -0,0 +1,84 @@
+;; helm-spotting.el
+;;
+;; based on (actually partly stolen from) helm-spotify.el package
+;;
+(require 'json)
+(require 'request)
+
+(defun alist-get (symbols alist)
+  "Look up the value for the chain of SYMBOLS in ALIST."
+  (if symbols
+      (alist-get (cdr symbols) (assoc (car symbols) alist)) (cdr alist)))
+
+(defmulti spotify-play-href (href)
+  "Get the Spotify app to play the object with the given HREF." system-type)
+
+(defmulti-method spotify-play-href 'darwin (href)
+  (shell-command (format "osascript -e 'tell application %S to play track %S'" "Spotify" href)))
+
+(defmulti-method spotify-play-href 'gnu/linux (href)
+  (shell-command (format "dbus-send --session --type=method_call --dest=com.spotify.qt / org.freedesktop.MediaPlayer2.OpenUri \"string:%s\"" href)))
+
+(defmulti-method spotify-play-href 'windows-nt (href)
+  (shell-command (format "explorer %S" href)))
+
+(defmulti-method-fallback spotify-play-href (href)
+  (message "Sorry, helm-spotify does not support playing tracks on %S." system-type))
+
+(defun spotify-play-track (track)
+  "Get the Spotify app to play the TRACK."
+  (spotify-play-href (alist-get '(uri) track)))
+
+(defun spotify-play-album (track)
+  "Get the Spotify app to play the album for this TRACK."
+  (spotify-play-href (alist-get '(album uri) track)))
+
+(defun helm-spotting-tracks (query-string)
+  (interactive "sQuery: ")
+  (spotify-search-async "track" query-string))
+
+(defun helm-spotting (query-string)
+  (interactive "sQuery: ")
+  (spotify-search-async "album" query-string))
+
+(defun spotify-format (js-data)
+  (mapcar (lambda (track) (cons (concat
+                                 " "
+                                 (alist-get '(name) (aref (alist-get '(artists) track) 0))
+                                 "  /  "
+                                 (alist-get '(album name) track)
+                                 "\n "
+                                 (alist-get '(name) track)
+                                 ) track))
+          (alist-get '(items) (alist-get '(tracks) data))
+          ))
+
+(defun helm-spotify-actions-for-track (actions track)
+  "Return a list of helm ACTIONS available for this TRACK."
+  `((,(format "Play Track - %s" (alist-get '(name) track)) . spotify-play-track)
+    (,(format "Play Album - %s" (alist-get '(name) track)) . spotify-play-album)
+    ("Show Track Metadata" . pp)))
+
+(defun spotify-search-async (endpoint search-string)
+  "Search spotify for SEARCH-TERM, and sends the results to helm."
+  (request
+   "https://api.spotify.com/v1/search"
+   :params `(("q" . ,search-string) ("type" . ,endpoint) ("limit" . "50"))
+   :parser 'json-read
+   :success (function*
+             (lambda (&key data &allow-other-keys)
+               (message "Ok")
+               (with-output-to-temp-buffer "*spotter debug*"
+                 (print data))
+               (helm :sources (helm-build-sync-source "Spotify tracks"
+                                :multiline t
+                                :candidates (spotify-format data)
+                                :action-transformer 'helm-spotify-actions-for-track
+                                )
+                :buffer "*helm spotter*")
+               ))
+   :error
+   (function* (lambda (&key error-thrown &allow-other-keys&rest _)
+                (setq helm-spotting-cache nil)
+                (message "Got error: %S" error-thrown)))
+   ))
--- init.el
+++ init.el
@@ -21,39 +21,36 @@
 
 ;; Required packages (rtags needs to be installed separately)
 (require 'package)
-(defvar package-list '(clang-format
-                       cmake-mode
-                       company
-                       company-jedi
-                       ethan-wspace
-                       flycheck
-                       highlight-symbol
-                       helm
-                       ido-ubiquitous
-                       ido-vertical-mode
-                       indent-guide
-                       json-mode
-                       json-reformat
-                       ;; magit (only on 24.4)
-                       markdown-mode+
-                       multiple-cursors
-                       nyan-mode
-                       popup
-                       powerline
-                       projectile
-                       py-autopep8
-                       rainbow-mode
-                       sx
-                       visual-regexp
-                       web-mode
-                       zenburn-theme))
+(defconst package-list '(clang-format
+                         cmake-mode
+                         company
+                         company-jedi
+                         ethan-wspace
+                         flycheck
+                         highlight-symbol
+                         helm
+                         json-mode
+                         json-reformat
+                         ;; magit (only on 24.4)
+                         markdown-mode+
+                         multiple-cursors
+                         nyan-mode
+                         popup
+                         powerline
+                         projectile
+                         py-autopep8
+                         rainbow-mode
+                         sx
+                         use-package
+                         visual-regexp
+                         web-mode
+                         zenburn-theme))
 
 (add-to-list 'package-archives
              '("melpa" . "https://melpa.org/packages/"))
 (add-to-list 'package-archives
              '("melpa-stable" . "https://stable.melpa.org/packages/"))
 (package-initialize)
-
 
 ;; Autoinstall packages
 (unless package-archive-contents
@@ -75,6 +72,60 @@
 ;; Common settings
 ;;
 
+
+;; HELM
+(require 'use-package)
+(require 'helm-config)
+
+(use-package helm-config
+             :config
+             (progn
+               (helm-mode 1)
+               (helm-adaptive-mode 1)
+               (helm-push-mark-mode 1)))
+
+;;; Helm-command-map
+;;
+;;
+(define-key helm-command-map (kbd "g")   'helm-apt)
+(define-key helm-command-map (kbd "w")   'helm-psession)
+(define-key helm-command-map (kbd "z")   'helm-complex-command-history)
+(define-key helm-command-map (kbd "w")   'helm-w3m-bookmarks)
+(define-key helm-command-map (kbd "x")   'helm-firefox-bookmarks)
+(define-key helm-command-map (kbd "#")   'helm-emms)
+(define-key helm-command-map (kbd "I")   'helm-imenu-in-all-buffers)
+
+;;; Global-map
+;;
+;;
+(global-set-key (kbd "M-x")                          'undefined)
+(global-set-key (kbd "M-x")                          'helm-M-x)
+(global-set-key (kbd "M-y")                          'helm-show-kill-ring)
+(global-set-key (kbd "C-c f")                        'helm-recentf)
+(global-set-key (kbd "C-x C-f")                      'helm-find-files)
+(global-set-key (kbd "C-c <SPC>")                    'helm-all-mark-rings)
+(global-set-key (kbd "C-x r b")                      'helm-filtered-bookmarks)
+(global-set-key (kbd "C-h r")                        'helm-info-emacs)
+(global-set-key (kbd "C-:")                          'helm-eval-expression-with-eldoc)
+(global-set-key (kbd "C-,")                          'helm-calcul-expression)
+(global-set-key (kbd "C-h i")                        'helm-info-at-point)
+(global-set-key (kbd "C-x C-d")                      'helm-browse-project)
+(global-set-key (kbd "<f1>")                         'helm-resume)
+(global-set-key (kbd "C-h C-f")                      'helm-apropos)
+(global-set-key (kbd "<f5> s")                       'helm-find)
+(global-set-key (kbd "<f2>")                         'helm-execute-kmacro)
+(global-set-key (kbd "C-c i")                        'helm-imenu-in-all-buffers)
+(global-set-key (kbd "<f11> o")                      'helm-org-agenda-files-headings)
+(global-set-key (kbd "C-s")                          'helm-occur)
+(define-key global-map [remap jump-to-register]      'helm-register)
+(define-key global-map [remap list-buffers]          'helm-buffers-list)
+(define-key global-map [remap dabbrev-expand]        'helm-dabbrev)
+(define-key global-map [remap find-tag]              'helm-etags-select)
+(define-key global-map [remap xref-find-definitions] 'helm-etags-select)
+(define-key global-map (kbd "M-g a")                 'helm-do-grep-ag)
+(define-key shell-mode-map (kbd "M-p")               'helm-comint-input-ring) ; shell history.
+
+;; (no)colors!
 (require 'zenburn-theme)
 
 ;; Some generic settings
@@ -130,8 +181,8 @@
 
 
 ;; Enable interactive autocompletion of files and commands
-(ido-mode 1)
-(ido-ubiquitous-mode 1)
+; (ido-mode 1)
+; (ido-ubiquitous-mode 1)
 (projectile-global-mode)
 (global-set-key (kbd "C-M-p") 'projectile-find-file)
 (global-set-key (kbd "C-<tab>") 'projectile-find-other-file)
@@ -210,8 +261,8 @@
 (defconst my-cc-style
   '("bsd"
     (c-offsets-alist . ((innamespace . [0])))
-    (c-basic-offset . 4)
-  ))
+    (c-basic-offset . 4)))
+
 (c-add-style "my-cc-style" my-cc-style)
 (setq c-default-style "my-cc-style")
 
@@ -319,8 +370,6 @@
           1 font-lock-warning-face t)))
   (highlight-symbol-mode)
   (rainbow-mode)
-  (indent-guide-mode)
-  (set-face-foreground 'indent-guide-face "#e09030")
   (hl-line-mode t)
   (company-mode)
   ;; Multiple cursors mode
@@ -331,13 +380,6 @@
 (add-hook 'prog-mode-hook 'my-prog-mode-hook)
 
 (set-face-foreground 'font-lock-warning-face "#F33")
-
-
-;; Setup indent guide to not slow down scrolling
-(setq indent-guide-delay 0.3)
-(setq indent-guide-recursive t)
-(setq indent-guide-char ":") ;; "┊")
-
 
 ;;; init.el ends here
 ;;
